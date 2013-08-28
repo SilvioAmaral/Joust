ec60a8598b2be7a9ba31ce538d5580b8db5d0a33
Silvio do Amaral e Silva <silvio.amaral@gmail.com>
2013-08-27 22:02:25 -0400

    Implementing full schema of database

--- models/db.js
+++ models/db.js
@@ -1,30 +1,61 @@
-var mongoose = require( 'mongoose' );
+var mongoose = require( 'mongoose' ),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
-var tournaments = new mongoose.Schema({
+var location = new mongoose.Schema({
+    name:{ type: String, required: true, trim: true },
+    address: { type: String, required: false, trim: true },
+    description: {type: String}
+});
+
+var user = new mongoose.Schema({
+    email: {type: String, required: true, trim: true},
+    first: {type: String, required: true, trim: true},
+    last: {type: String, required: true, trim: true},
+    getNews: {type: Boolean, required: true, trim: true},
+    description: {type: String}
+});
+
+var competitor = new mongoose.Schema({
+    email: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true },
+    type: {type: String, enum: ['Team', 'Individual']},
+    users: [user],
+    Description: {type: String}
+});
+
+var result = new mongoose.Schema({
+    type: {type: String, enum:['Win', 'Loss', 'Tie', 'Other'], required: true},
+    points: {type: String, Required: false},
+    description: {type: String}
+});
+
+
+var match = new mongoose.Schema({
+    date: {type: String, required: true},
+    location: [location],
+    competitors: [competitor],
+    result: {type: ObjectId, ref: 'result'},
+    description: {type: String},
+    status: {type: String, enum: ['Pending', 'In Game', 'Done', 'Cancelled']}
+});
+
+
+var tournament = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
     start_date: { type: String, required: false },
     end_date: { type: String, required: false },
     type: { type: String, required: true, trim: true },
-    matches: { type: String, required: false }
-    // TODO: change types to appropriate types, and add users
+    matches: [match],
+    Description: {type: String}
 });
 
-var competitors = new mongoose.Schema({
-    email: { type: String, required: true, trim: true },
-    name: { type: String, required: true, trim: true },
-    type: {type: String, enum: ['Team', 'Individual']},
-    users: [users]
-    // TODO: Add property for List of emails in case of team
-});
 
-var users = new mongoose.Schema({
-    email: {type: String, required: true, trim: true},
-    first: {type: String, required: true, trim: true},
-    last: {type: String, required: true, trim: true},
-    getNews: {type: Boolean, required: true, trim: true}
-});
-
-mongoose.model( 'tournament', tournaments );
-mongoose.model( 'competitor', competitors );
+mongoose.model( 'tournament', tournament );
+mongoose.model( 'match', match );
+mongoose.model( 'competitor', competitor );
+mongoose.model( 'user', user );
+mongoose.model( 'result', result );
+mongoose.model( 'location', location );
 
 mongoose.connect( 'mongodb://localhost:27017/joust' );
